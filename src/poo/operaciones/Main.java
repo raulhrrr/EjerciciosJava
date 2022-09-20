3c5cd383c72230cdfd66562c4a345ace33a9e7af
Raúl Herrera <raul.herrerac@uniagustiniana.edu.co>
2022-09-19 19:39:58 -0500

    POO files added

---
+++ src/poo/operaciones/Main.java
@@ -0,0 +1,16 @@
+package poo.operaciones;
+
+public class Main {
+    public static void main(String[] args) {
+        Operacion operacion = new Operacion();
+        
+        operacion.leerNumeros();
+        operacion.sumar();
+        operacion.restar();
+        operacion.multiplicar();
+        operacion.dividir();
+        operacion.elevado();
+        operacion.mostrarResultado();
+        
+    }
+}
---
+++ src/poo/operaciones/Operacion.java
@@ -0,0 +1,48 @@
+package poo.operaciones;
+
+import javax.swing.JOptionPane;
+
+public class Operacion {
+
+    float numeroUno;
+    float numeroDos;
+    float resultadoSuma;
+    float resultadoResta;
+    float resultadoMultiplicacion;
+    float resultadoDivision;
+    float resultadoExponenciacion;
+    
+    public void leerNumeros() {
+        numeroUno = Integer.parseInt(JOptionPane.showInputDialog("Digite el primer numero: "));
+        numeroDos = Integer.parseInt(JOptionPane.showInputDialog("Digite el segundo numero: "));
+    }
+    
+    public void sumar() {
+        resultadoSuma = numeroUno + numeroDos;
+    }
+    
+    public void restar() {
+        resultadoResta = numeroUno - numeroDos;
+    }
+    
+    public void multiplicar() {
+        resultadoMultiplicacion = numeroUno * numeroDos;
+    }
+    
+    public void dividir() {
+        resultadoDivision = numeroUno / numeroDos;
+    }
+    
+    public void elevado() {
+        resultadoExponenciacion = (float) Math.pow(numeroUno, numeroDos);
+    }
+    
+    public void mostrarResultado() {
+        System.out.println("La suma es: " + resultadoSuma);
+        System.out.println("La resta es: " + resultadoResta);
+        System.out.println("La multiplicacion es: " + resultadoMultiplicacion);
+        System.out.println("La division es: " + resultadoDivision);
+        System.out.println("La exponenciacion es: " + resultadoExponenciacion);
+    }
+    
+}
